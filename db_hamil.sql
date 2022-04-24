850d693a91d081ba3ad95939432674e5594506ed
Atika Marlinda <atika320@mhs.unsyiah.ac.id>
2022-04-24 10:55:42 +0700

    db untuk login

---
+++ db_hamil.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 20 Apr 2022 pada 18.24
+-- Versi server: 10.4.21-MariaDB
+-- Versi PHP: 8.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_hamil`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `dbuser`
+--
+
+CREATE TABLE `dbuser` (
+  `iduser` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `nm_user` varchar(100) NOT NULL,
+  `level` int(11) NOT NULL,
+  `last_log` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `dbuser`
+--
+
+INSERT INTO `dbuser` (`iduser`, `username`, `password`, `nm_user`, `level`, `last_log`) VALUES
+(1, 'admin', '123', 'rini', 1, '2022-04-20 22:49:08');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `dbuser`
+--
+ALTER TABLE `dbuser`
+  ADD PRIMARY KEY (`iduser`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `dbuser`
+--
+ALTER TABLE `dbuser`
+  MODIFY `iduser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
