40d6d23531dc118036afd7de645bda341516de88
aydiani <101510294+aydiani@users.noreply.github.com>
2022-05-27 20:52:20 +0700

    Delete Aplikasi konsultasi ibu hamil versi 1.0 directory

--- Aplikasi konsultasi ibu hamil versi 1.0/README.md
+++
@@ -1,2 +0,0 @@
-# TIK204-2022-Kelompok-03-Sistem-Antrian-Online-Konsultasi-Ibu-Hamil
-Repositori Baru Untuk Sistem Antrian Online Konsultasi Ibu Hamil
--- Aplikasi konsultasi ibu hamil versi 1.0/pages/samples/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host="localhost";
-$username="root";
-$password="";
-$database="db_hamil";
-
-$connection = mysqli_connect($host,$username,$password,$database);
-?>
