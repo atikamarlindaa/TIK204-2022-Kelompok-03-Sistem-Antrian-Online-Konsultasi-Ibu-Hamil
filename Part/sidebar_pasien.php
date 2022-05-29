6d43d0840a98fa2ac0b77151f65951e734f3924c
Atika Marlinda <atika320@mhs.unsyiah.ac.id>
2022-05-29 22:41:29 +0700

    update sidebar

--- Part/sidebar_pasien.php
+++ Part/sidebar_pasien.php
@@ -7,17 +7,17 @@ foreach ($pecahjudul as $w) {
   $acronym .= $w[0];
 }
 ?>
-<div class="main-sidebar sidebar-style-2">
+<div class="main-sidebar sidebar-style-1">
   <aside id="sidebar-wrapper">
     <div class="sidebar-brand">
-      <img src="assets/img/logoprecons.png" alt="logo" width="35" class="rounded-box">
-      <a href="http://localhost/PRECONS/dash_pasien.php"><?php echo $judul; ?></a>
+      <img src="assets/img/logoprecons3.png" alt="logo" width="35" class="rounded-box">
+      <a href="http://localhost/PRECONS/dashboard_pasien.php"><?php echo $judul; ?></a>
     </div>
     <div class="sidebar-brand sidebar-brand-sm">
       <a href="index.html"><?php echo $acronym; ?></a>
     </div>
     <ul class="sidebar-menu">
-      <li <?php echo ($page == "Dashboard Pasien") ? "class=active" : ""; ?>><a class="nav-link" href="dash_pasien.php"><i class="fas fa-fire"></i><span>Dashboard</span></a></li>
+      <li <?php echo ($page == "Dashboard Pasien") ? "class=active" : ""; ?>><a class="nav-link" href="dashboard_pasien.php"><i class="fas fa-fire"></i><span>Dashboard</span></a></li>
       <li <?php echo ($page == "Jadwal Dokter") ? "class=active" : ""; ?>><a href="jadwaldokter_pasien.php" class="nav-link"><i class="fas fa-users"></i> <span>Jadwal Dokter</span></a></li>
 
 
@@ -25,6 +25,6 @@ foreach ($pecahjudul as $w) {
             
       
        <li <?php echo ($page == "Data Foto Hasil USG" || @$page1 == "detrot") ? "class=active" : ""; ?>><a class="nav-link" href="usg_pasien.php"><i class="fas fa-skull"></i> <span>Foto Hasil USG</span></a></li>
-      <li <?php echo ($page == "Data Diri" || @$page1 == "det") ? "class=active" : ""; ?>><a class="nav-link" href="riwayat_pasien.php"><i class="fas fa-user-injured"></i> <span>Data Diri</span></a></li>
+      <li <?php echo ($page == "Data Diri" || @$page1 == "det") ? "class=active" : ""; ?>><a class="nav-link" href="profil_pasien.php"><i class="fas fa-user-injured"></i> <span>Data Diri</span></a></li>
   </aside>
 </div>
