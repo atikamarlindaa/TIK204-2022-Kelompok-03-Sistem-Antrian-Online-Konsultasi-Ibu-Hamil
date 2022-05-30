b16b906aa1f39331c35be496c2aab0660a06245d
Atika Marlinda <atika320@mhs.unsyiah.ac.id>
2022-05-30 08:52:05 +0700

    update sidebar admin

--- Part/sidebar_admin.php
+++ Part/sidebar_admin.php
@@ -22,10 +22,8 @@ foreach ($pecahjudul as $w) {
       </li></li>
       </li></li>
       <li <?php echo ($page == "Dashboard Admin") ? "class=active" : ""; ?>><a class="nav-link" href="dashboard_admin.php"><i class="fas fa-fire"></i><span>Dashboard</span></a></li>
-
       <li <?php echo ($page == "Rekam Medis") ? "class=active" : ""; ?>><a class="nav-link" href="rekam_medis.php"><i class="fas fa-stethoscope"></i> <span>Rekam Medis</span></a></li>
       <li <?php echo ($page == "Jadwal Dokter") ? "class=active" : ""; ?>><a href="pegawai_admin.php" class="nav-link"><i class="fas fa-users"></i> <span>Jadwal Dokter</span></a></li>
-
       <li <?php echo ($page == "Data Pasien" || @$page1 == "det") ? "class=active" : ""; ?>><a class="nav-link" href="antrian_admin.php"><i class="fas fa-user-injured"></i> <span>Data Pasien</span></a></li>
       </li>
       <li <?php echo ($page == "Data Vitamin") ? "class=active" : ""; ?>><a class="nav-link" href="obat_admin.php"><i class="fas fa-briefcase-medical"></i> <span>Obat/Vitamin</span></a></li>
