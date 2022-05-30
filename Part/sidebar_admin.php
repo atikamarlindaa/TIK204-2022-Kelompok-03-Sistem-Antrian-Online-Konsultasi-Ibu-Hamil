d02dee8014e4291652ea33d128667ae52876ca0f
Atika Marlinda <atika320@mhs.unsyiah.ac.id>
2022-05-30 08:44:12 +0700

    sidebar perspektif admin

--- Part/sidebar_admin.php
+++ Part/sidebar_admin.php
@@ -8,11 +8,11 @@ foreach ($pecahjudul as $w) {
 }
       
 ?>
-<div class="main-sidebar sidebar-style-2">
+<div class="main-sidebar sidebar-style-1">
   <aside id="sidebar-wrapper ">
     <div class="sidebar-brand">
-      <img src="assets/img/logoprecons.png" alt="logo" width="35" class="rounded-box">
-      <a href="http://localhost/PRECONS/dash_admin.php"><?php echo $judul; ?></a>
+      <img src="assets/img/logoprecons3.png" alt="logo" width="35" class="rounded-box">
+      <a href="http://localhost/PRECONS/dashboard_admin.php"><?php echo $judul; ?></a>
     </div>
     <div class="sidebar-brand sidebar-brand-sm">
       
@@ -21,14 +21,13 @@ foreach ($pecahjudul as $w) {
     <ul class="sidebar-menu">
       </li></li>
       </li></li>
-      <li <?php echo ($page == "Dashboard Admin") ? "class=active" : ""; ?>><a class="nav-link" href="dash_admin.php"><i class="fas fa-fire"></i><span>Dashboard</span></a></li>
+      <li <?php echo ($page == "Dashboard Admin") ? "class=active" : ""; ?>><a class="nav-link" href="dashboard_admin.php"><i class="fas fa-fire"></i><span>Dashboard</span></a></li>
 
       <li <?php echo ($page == "Rekam Medis") ? "class=active" : ""; ?>><a class="nav-link" href="rekam_medis.php"><i class="fas fa-stethoscope"></i> <span>Rekam Medis</span></a></li>
-      <li <?php echo ($page == "Data Pasien" || @$page1 == "det") ? "class=active" : ""; ?>><a class="nav-link" href="pasien_book_admin.php"><i class="fas fa-user-injured"></i> <span>Data Pasien</span></a></li>
+      <li <?php echo ($page == "Jadwal Dokter") ? "class=active" : ""; ?>><a href="pegawai_admin.php" class="nav-link"><i class="fas fa-users"></i> <span>Jadwal Dokter</span></a></li>
 
-      <li <?php echo ($page == "Jadwal Dokter") ? "class=active" : ""; ?>><a href="jadwaldokter_admin.php" class="nav-link"><i class="fas fa-users"></i> <span>Jadwal Dokter</span></a></li>
+      <li <?php echo ($page == "Data Pasien" || @$page1 == "det") ? "class=active" : ""; ?>><a class="nav-link" href="antrian_admin.php"><i class="fas fa-user-injured"></i> <span>Data Pasien</span></a></li>
       </li>
-      <li <?php echo ($page == "Data Foto Hasil USG" || @$page1 == "detrot") ? "class=active" : ""; ?>><a class="nav-link" href="usg_admin.php"><i class="fas fa-skull"></i> <span>Foto Hasil USG</span></a></li>
-      <li <?php echo ($page == "Data Vitamin") ? "class=active" : ""; ?>><a class="nav-link" href="obat_admin.php"><i class="fas fa-briefcase-medical"></i> <span>Vitamin</span></a></li>
+      <li <?php echo ($page == "Data Vitamin") ? "class=active" : ""; ?>><a class="nav-link" href="obat_admin.php"><i class="fas fa-briefcase-medical"></i> <span>Obat/Vitamin</span></a></li>
   </aside>
 </div>
