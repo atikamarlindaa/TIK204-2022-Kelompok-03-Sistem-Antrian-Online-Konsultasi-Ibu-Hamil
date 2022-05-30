2a5c4ac24ff75ce95c641be45293ce11009d488b
Atika Marlinda <atika320@mhs.unsyiah.ac.id>
2022-05-30 08:30:33 +0700

    pasien ambil antrian

---
+++ antrian_admin.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php
+  $page = "Data Pasien";
+  session_start();
+  include 'auth/connect.php';
+  include "part/head.php";
+  include "part_func/tgl_ind.php";
+  include "part_func/umur.php";
+
+  if (isset($_POST['submit'])) {
+    $id = $_POST['id'];
+    $nama = $_POST['nama'];
+    $berat = $_POST['berat'];
+    $tinggi = $_POST['tinggi'];
+    $tgl = $_POST['tgl'];
+
+    $up2 = mysqli_query($conn, "UPDATE pasien SET nama_pasien='$nama', tgl_lahir='$tgl', berat_badan='$berat', tinggi_badan='$tinggi' WHERE id='$id'");
+    echo '<script>
+				setTimeout(function() {
+					swal({
+					title: "Data Diubah",
+					text: "Data Pasien berhasil diubah!",
+					icon: "success"
+					});
+					}, 500);
+				</script>';
+  }
+  ?>
+</head>
+
+<body>
+  <div id="app">
+    <div class="main-wrapper main-wrapper-1">
+      <div class="navbar-bg"></div>
+
+      <?php
+      include 'part/navbar_admin.php';
+      include 'part/sidebar_admin.php';
+      ?>
+
+      <!-- Main Content -->
+      <div class="main-content">
+        <section class="section">
+          <div class="section-header">
+            <h1><?php echo $page; ?></h1>
+          </div>
+          <div class="section-body">
+            <div class="row">
+              <div class="col-12">
+                <div class="card">
+                  <div class="card-header">
+                    <h4>Pasien yang telah mengambil antrian</h4>
+                  </div>
+                  <div class="card-body">
+                    <div class="table-responsive">
+                      <table class="table table-striped" id="table-1">
+                        <thead>
+                          <tr>
+                            <th class="text-center">#</th>
+                            <th>Nama</th>
+                            <th>Dokter yang dipilih</th>
+                            <th>Tanggal Antrian</th>
+                            <th>Waktu Antrian</th>
+                            <th>Fasilitas</th>
+                            <th>Action</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <?php
+                          $sql = mysqli_query($conn, "SELECT * FROM antrian");
+                          $i = 0;
+                          while ($row = mysqli_fetch_array($sql)) {
+                            $idpasien = $row['id'];
+                            $i++;
+                          ?>
+                            <tr>
+                              <td><?php echo $i; ?></td>
+                              <th><?php echo ucwords($row['nama_pasien']); ?>
+                              </th>
+                              <td><?php echo $row['dokter_pilih'] ?></td>
+                              <td><?php echo $row['tanggal']?></td>
+                              <td><?php echo $row['pukul']?></td>
+                              <td><?php echo $row['fasilitas']?></td>
+                              <td>
+                              </span>
+                              </span>
+                                  <a class="btn btn-danger btn-action mr-1" data-toggle="tooltip" title="Hapus" data-confirm="Hapus Data|Apakah anda ingin menghapus data ini?" data-confirm-yes="window.location.href = 'auth/delete_admin.php?type=antrian&id=<?php echo $row['id']; ?>'" ;><i class="fas fa-trash"></i></a>
+                            </td>
+                            </tr>
+                          <?php } ?>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+
+      <div class="modal fade" tabindex="-1" role="dialog" id="editPasien">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">Edit Data</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <form action="" method="POST" class="needs-validation" novalidate="">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">Nama Pasien</label>
+                  <div class="col-sm-9">
+                    <input type="hidden" class="form-control" name="id" required="" id="getId">
+                    <input type="text" class="form-control" name="nama" required="" id="getNama">
+                    <div class="invalid-feedback">
+                      Mohon data diisi!
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">Tanggal lahir</label>
+                  <div class="form-group col-sm-9">
+                    <input type="text" class="form-control datepicker" id="getTgl" name="tgl">
+                  </div>
+                </div>
+            </div>
+            <div class="modal-footer bg-whitesmoke br">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="submit" class="btn btn-primary" name="submit">Edit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <?php include 'part/footer.php'; ?>
+    </div>
+  </div>
+  <?php include "part/all-js.php"; ?>
+
+  <script>
+    $('#editPasien').on('show.bs.modal', function(event) {
+      var button = $(event.relatedTarget)
+      var nama = button.data('nama')
+      var id = button.data('id')
+      var tgl = button.data('lahir')
+      var berat = button.data('berat')
+      var tinggi = button.data('tinggi')
+      var modal = $(this)
+      modal.find('#getId').val(id)
+      modal.find('#getNama').val(nama)
+      modal.find('#getTgl').val(tgl)
+      modal.find('#getBerat').val(berat)
+      modal.find('#getTinggi').val(tinggi)
+    })
+  </script>
+</body>
+
+</html>
